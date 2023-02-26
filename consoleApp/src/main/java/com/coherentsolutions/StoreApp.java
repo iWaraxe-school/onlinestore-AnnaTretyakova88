50d4aaf148e2dc406c6dc967bf5dc665e76975e5
AnnaTretyakova <annatretyakova@coherentsolutions.com>
2023-02-27 00:04:28 +0300

    Console Interaction

--- consoleApp/src/main/java/com/coherentsolutions/StoreApp.java
+++ consoleApp/src/main/java/com/coherentsolutions/StoreApp.java
@@ -2,12 +2,41 @@ package com.coherentsolutions;
 
 import com.coherentsolutions.store.RandomStorePopulator;
 import com.coherentsolutions.store.Store;
+import java.io.Console;
 
 public class StoreApp {
     public static void main(String[] args) {
         Store store = new Store();
         RandomStorePopulator populator = new RandomStorePopulator(store);
         populator.fillByProducts();
-        store.printStore();
+        // there should be while loop with readline
+
+        Console console = System.console();
+
+        System.out.println("Welcome in Anna Shop! \n");
+        System.out.println("Type All, Sort, Top5 or Quit");
+
+        String command = console.readLine();
+        boolean stop = false;
+        while (!stop) {
+
+            switch (command) {
+                case "All":
+                    store.printStore();
+                case "Sort":
+                    store.printSortedProducts();
+                    break;
+                case "Top5":
+                    store.printTopProducts();
+                    break;
+                case "Quit":
+                    stop = true;
+                    break;
+                default:
+                    System.out.println("Invalid option");
+                    break;
+            }
+        }
     }
 }
+
--- store/src/main/java/com/coherentsolutions/store/Store.java
+++ store/src/main/java/com/coherentsolutions/store/Store.java
@@ -1,9 +1,12 @@
 package com.coherentsolutions.store;
 
 import com.coherentsolutions.domain.Category;
-import com.coherentsolutions.domain.CategoryNames;
+import com.coherentsolutions.domain.Product;
+import com.coherentsolutions.store.sorting.ProductComparator;
+import com.coherentsolutions.store.sorting.XMLParser;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Store {
@@ -21,5 +24,26 @@ public class Store {
         for (Category category : categoryItems) {
             System.out.println(category);
         }
+    }
+    public List<Product> getAllProducts (){
+        List<Product> allProducts = new ArrayList<>();
+        for (Category category: categoryItems) {
+            allProducts.addAll(category.getProducts());
+        }
+        return allProducts;
+    }
+    public void printSortedProducts (){
+        List<Product> allProducts = getAllProducts();
+        allProducts.sort(ProductComparator.buildComparator(XMLParser.readAndParseXML()));
+        for (Product product: allProducts) {
+            System.out.println(product);
+        }
+    }
+    public void printTopProducts (){
+        List<Product> allProducts = getAllProducts();
+        allProducts.sort(Comparator.comparing(Product::getPrice).reversed());
+        allProducts.stream()
+                .limit(5)
+                .forEach(System.out::println);
     }
 }
